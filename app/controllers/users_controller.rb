93fab60e36bc7ac9600f5eedc5c8864f2925d8d6
tibraj <tibraj94@gmail.com>
2020-11-11 11:30:15 -0500

    whitelist user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,8 @@ class UsersController < ApplicationController
     def set_user
         @user = User.find(params[:id])
     end
+
+    def user_params 
+        params.require(:user).permit(:name, :username, :password)
+    end
 end
