e360b48e91883df01346797700de83a33c2a4e32
tibraj <tibraj94@gmail.com>
2020-11-12 14:08:21 -0500

    sessions create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
-    
+    def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            render json: @user, status: :ok
+        else 
+            render json: {error: "Invalid Entry"}
+        end 
+    end
 end
