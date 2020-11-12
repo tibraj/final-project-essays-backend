3c3f4e14f428eac7a615643b506d711d7bd1b0a8
tibraj <tibraj94@gmail.com>
2020-11-12 13:05:18 -0500

    signup route to create users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :password_digest
+  attributes :id, :name, :username
   has_many :essays
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+first_test = User.create(name: "Taulant", username: "TaulantI", password: "password")
+first_essay_test = Essay.create(title: "Title 1", content: "Content 1", user_id: 1)
+second_test = User.create(name: "Blendi", username: "BlendiI", password: "password")
+second_essay_test = Essay.create(title: "Title 2", content: "Content 2", user_id: 2)
