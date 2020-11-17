6002b6edfc7175f49a4d7d9a462a29c7661847d8
tibraj <tibraj94@gmail.com>
2020-11-16 19:37:51 -0500

    route to get current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post "/api/v1/signup", to: "api/v1/users#create"
   post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/current_user" to: "api/v1/sessions#get_current_user"
   namespace :api do 
     namespace :v1 do
       resources :users
