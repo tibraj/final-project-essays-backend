39cd142b1bb1d933e4822af7d6920cb7ee0d6575
tibraj <tibraj94@gmail.com>
2020-11-18 16:14:30 -0500

    checks for current user before each action

--- app/controllers/api/v1/essays_controller.rb
+++ app/controllers/api/v1/essays_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::EssaysController < ApplicationController
+    before_action :logged_in?
     def index
         @essays = Essay.all
         render json: @essays
     end
 
     def create
-        
+        @essay = current_user
     end
 
     private 
