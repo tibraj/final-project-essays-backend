937af76913e4242ac4b5370b2294a1cb13bff871
tibraj <tibraj94@gmail.com>
2020-11-20 14:16:14 -0500

    essay show action

--- app/controllers/api/v1/essays_controller.rb
+++ app/controllers/api/v1/essays_controller.rb
@@ -6,9 +6,23 @@ class Api::V1::EssaysController < ApplicationController
     end
 
     def create
-        @essay = current_user
+        @essay = current_user.essays.build(essay_params)
+        if @essay.save
+            render json: @essay, status: :created
+        else 
+            response = {
+                error: essay.errors.full_messages.to_sentence
+            }
+            render json: response, status: :unprocessable_entity
+        end 
     end
 
+    def show 
+        @essay = Essay.find(params[:id])
+        render json: @essay 
+    end
+
+    
     private 
 
     def essay_params
